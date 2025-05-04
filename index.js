04fbe2dc2ac8acde0f4147cadd1cf5371b378e75
Jitender-rana <119347955+Jitender-rana@users.noreply.github.com>
2025-05-04 23:53:07 +0530

    removing express server

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ import express from "express";
 const app=express();
 const port=3000;
 console.log("This is me testing jenkins");
-app.listen(port,()=>{
-    console.log(`server listening on port ${port}`)
-})
+
